91d67b75695e1a10b0bf1693525dff83eb8ee5a6
agatha-yin <agathayin.sh@gmail.com>
2023-10-29 00:49:51 -0400

    messages to csv

--- background.js
+++ background.js
@@ -1,31 +1,13 @@
 chrome.runtime.onInstalled.addListener(() => {
   console.log("Extension running");
-  chrome.storage.sync.set({ paiMessages: [] });
 });
 
 var messages = [];
-
-function download(data) {
-  let blob = new Blob(data, { type: "text/plain;charset=UTF-8" });
-  let url = URL.createObjectURL(blob);
-  chrome.downloads.download({ url: url });
-}
-
-function addMessage(msg) {
-  chrome.storage.sync.get("paiMessages").then((result) => {
-    let messages = [];
-    if (result.paiMessages && result.paiMessages.length) messages = result.paiMessages;
-
-    messages.unshift(msg);
-    chrome.storage.sync.set({ paiMessages: messages });
-  });
-}
 
 chrome.runtime.onMessage.addListener(async function (request, sender, sendResponse) {
   if (request.message !== null && request.message == "start_request") {
     try {
       // clear records
-      chrome.storage.sync.set({ paiMessages: [] });
       messages = [];
       // create tab
       let newTab = await chrome.tabs.create({
@@ -34,28 +16,21 @@ chrome.runtime.onMessage.addListener(async function (request, sender, sendRespon
       await new Promise((resp) => setTimeout(resp, 3000));
       if (!newTab || !newTab.id) {
         console.log("Task failed. Cannot create tab");
-        addMessage("Task failed. Cannot create tab");
         return;
       }
       // check if logged in
       let userName = await chrome.tabs.sendMessage(newTab.id, {
         message: "login",
       });
-      if (userName) {
-        addMessage("Logged as " + userName);
-      } else {
-        addMessage("Please Log in first.");
-        return;
-      }
       // find max page
       let pageUrlList = await chrome.tabs.sendMessage(newTab.id, { message: "getPageUrlList" });
       if (pageUrlList.message || !pageUrlList.result) {
-        addMessage(urls.message);
         return;
       }
       // check all pages
       let pages = pageUrlList.result;
-      console.log(pages);
+      console.log("page urls", pages);
+
       for (let url of pages) {
         let msgTab = await chrome.tabs.create({
           url: url,
@@ -64,21 +39,76 @@ chrome.runtime.onMessage.addListener(async function (request, sender, sendRespon
         if (msgTab && msgTab.id) {
           let result = await chrome.tabs.sendMessage(msgTab.id, { message: "getMessages" });
           if (result && result.length) {
-            console.log(result);
             messages.push(...result);
           }
           try {
             await chrome.tabs.remove(msgTab.id);
-          } catch (err) {}
+          } catch (err) {
+            console.log("failed to close tab for " + url);
+          }
         }
       }
-      // format data into csv file and download
-      download(messages.join("\n"));
+      console.log("messages", messages);
+      // format data into csv file
+      const headers = [
+        "评分者",
+        "影响",
+        "帖子",
+        "发表日期",
+        "所在版块",
+        "操作时间",
+        "操作理由",
+        "影响类别",
+        "影响分数",
+      ];
+      let content = messages.map((el) => {
+        let line = el.split("\n");
+        line = line.slice(0, line.length - 1);
+        line = line.map((el, i) => {
+          let index = el.indexOf("：");
+          if (index > -1) {
+            return el.slice(index + 1, el.length);
+          } else {
+            return el;
+          }
+        });
+        let influence = line[1].split(":");
+        let iType = influence[0] || "";
+        let iScore = influence[1] || "";
+        line.push(iType, iScore);
+        return line.join(",");
+      });
+      content.unshift(headers);
+      content = content.join("\n");
+
+      // download csv file
+      // for utf8 bom
+      const data = "\uFEFF" + content;
+      const blob = new Blob([data], { type: "text/csv;charset=utf-8" });
+
+      // use BlobReader object to read Blob data
+      const reader = new FileReader();
+      reader.onload = () => {
+        const buffer = reader.result;
+        const blobUrl = `data:${blob.type};base64,${btoa(
+          new Uint8Array(buffer).reduce((data, byte) => data + String.fromCharCode(byte), "")
+        )}`;
+        chrome.downloads.download(
+          {
+            url: blobUrl,
+            filename: request.filename,
+            saveAs: true,
+            conflictAction: "uniquify",
+          },
+          () => {
+            sendResponse({ success: true });
+          }
+        );
+      };
+      reader.readAsArrayBuffer(blob);
+      return true;
     } catch (err) {
       console.log(err);
     }
-    // reset all variables
-
-    // start request
   }
 });
--- popup.js
+++ popup.js
@@ -3,18 +3,9 @@ function loadnr() {
   chrome.runtime.sendMessage({ message: "start_request" });
 }
 
-function loadMessage() {
-  chrome.storage.sync.get("paiMessages", ({ data }) => {
-    console.log(data);
-    const contentHTML = data.map((el) => `<p>${el}</p>`).join("");
-    document.querySelector("#message").innerHTML = contentHTML;
-  });
-}
-
 // onload
 document.addEventListener("DOMContentLoaded", function () {
   document.getElementById("start_request").addEventListener("click", loadnr);
-  document.getElementById("load_message").addEventListener("click", loadMessage);
 });
 
 loadMessage();
